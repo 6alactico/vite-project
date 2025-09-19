0bb3cf3b48023b14190b88f9c4f415200eba55e1
Craigz <6alactico>
2025-09-18 23:20:15 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,35 @@ import { useWindowSize } from '@react-hook/window-size';
 
 function App() {
   const [showMessage, setShowMessage] = useState(false);
+  const [showVideo, setShowVideo] = useState(false);
+
+  useEffect(() => {
+    console.log("🎯 Someone opened the page!");
+  }, []);
+
 
   function handleClick() {
-    setShowMessage(!showMessage);
+    setShowMessage(true);
+    setShowVideo(true);
   }
 
   return (
-      <div>
-        {showMessage && <ConfettiComponent />}
-        {!showMessage && (
+      <section>
+        {showMessage && <ConfettiComponent showVideo={showVideo}/>}
+        {!showMessage && !showVideo && (
           <button onClick={handleClick}>Tap Me!</button>
         )}
-      </div>
+      </section>
   )
 }
 
-function ConfettiComponent() {
-  const [width, height] = useWindowSize();
+function ConfettiComponent({ showVideo }) {
   const [showConfetti, setShowConfetti] = useState(true);
   const [fadeOut, setFadeOut] = useState(false);
 
   useEffect(() => {
-    const fadeTimer = setTimeout(() => setFadeOut(true), 4000);
-    const removeTimer = setTimeout(() => setShowConfetti(false), 5000);
+    const fadeTimer = setTimeout(() => setFadeOut(true), 4000); 
+    const removeTimer = setTimeout(() => setShowConfetti(false), 7000); 
 
     return () => {
       clearTimeout(fadeTimer);
@@ -36,12 +42,13 @@ function ConfettiComponent() {
   }, []);
 
   return (
-    <div>
+    <div className='content'>
       <div className={`confetti-container ${fadeOut ? 'fade-out' : ''}`}>
-        {showConfetti && <Confetti width={width} height={height}/>}
+        {showConfetti && <Confetti width={375} height={812}/>}
       </div>
       <h1>Happy Friday!</h1>
-      <VideoPlayer />
+      {showVideo && <VideoPlayer />}
+      <span className='emoji'>🤣🤣🤣</span>
     </div>
   )
 }
@@ -49,12 +56,11 @@ function ConfettiComponent() {
 function VideoPlayer() {
   return (
     <iframe
-      width="560"
-      height="315"
       src="https://www.youtube.com/embed/9XLjz3OMUPI?si=nFC9FM9MsMnVL5h7&autoplay=1&loop=1&playlist=9XLjz3OMUPI"
       title="YouTube video"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       allowFullScreen
+      playsInline
       frameBorder="0"
     />
   );
